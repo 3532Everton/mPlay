559b19c74e7bc325e4468fd9f513f5c9fd56f18c
Everton Martins <3532everton@gmail.com>
2022-04-21 17:18:11 -0300

    metronomo completo

--- js/metronomo.js
+++ js/metronomo.js
@@ -2,12 +2,33 @@
 const metronomoBnt = document.getElementById('metronomo-bnt')
 metronomoBnt.addEventListener('click', function(){
     const bntIconPlayPause = document.getElementById('bnt-icon-play-pause')
-    metronomoBnt.classList.toggle('iniciado')
-    if (metronomoBnt.classList.contains('iniciado')){
-        bntIconPlayPause.classList.remove('fa-play')
-        bntIconPlayPause.classList.add('fa-pause')
-    } else {
-        bntIconPlayPause.classList.remove('fa-pause')
-        bntIconPlayPause.classList.add('fa-play')
-    }
+    bntIconPlayPause.classList.remove('fa-play')
+    bntIconPlayPause.classList.add('fa-pause')
+    metronomoBnt.style.background = '#161616'
+    const attr = document.createAttribute('disabled')
+    metronomoBnt.setAttributeNode(attr)
+    tocar(true)
 })
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+function tocar(condicao){
+    const tempo = document.getElementById('metronomo-tempo')
+    const metronomoAudio = document.getElementById('metronomo-audio')
+    const bntIconPlayPause = document.getElementById('bnt-icon-play-pause')
+
+    var tocando = setInterval(() => {
+        metronomoAudio.play()
+
+        setTimeout(() => {
+            clearInterval(tocando)
+            metronomoBnt.removeAttribute('disabled')
+            metronomoBnt.style.background = '#B5B5B5'
+            bntIconPlayPause.classList.remove('fa-pause')
+            bntIconPlayPause.classList.add('fa-play')
+            
+        }, tempo.value*1000);
+    }, 1000);
+}
